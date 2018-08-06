49c05e44a7d9224514de6fcba8a3d43d2d7d46fa
Ryan <hiRyan@protonmail.com>
2018-08-06 20:36:44 +0300

    We can add a user to our bank

--- src/com/bank/Bank.java
+++ src/com/bank/Bank.java
@@ -1,11 +1,33 @@
 package com.bank;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class Bank {
 
-    private String name;
+    //properties
+    private List<User> users;
+    private String bankName;
 
-    private ArrayList<User> clients;
+    // constructor
+    public Bank(String bankName) {
+        this.bankName = bankName;
+    }
+
+    //methods
+
+    //user comes to the bank and we create a new user
+    public void addUser(String password, String fullName, String address,
+                        String phoneNumber, String email, String secretQuestion,
+                        String answerToSecretQuestion) {
+        User newUser = new User(password,
+                fullName, address, phoneNumber, email, secretQuestion, answerToSecretQuestion);
+        users.add(newUser);
+    }
+
+    //if user already exists from another bank and we add them to this bank branch
+    public void addUser(User user) {
+        users.add(user);
+    }
+
 
 }
